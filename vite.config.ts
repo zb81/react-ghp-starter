6976317360ad0b6d2c178ea1db9b3ae963c5ff6a
朱备 <zbneversettle@gmail.com>
2023-12-19 14:19:21 +0800

    chore: First commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-ghp-starter',
-  plugins: [vue()],
+  base: '/react-ghp-starter',
+  plugins: [react()],
 })
