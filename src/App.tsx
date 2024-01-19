c1b8c5888a2b9f18d9da86aec43a3ffbde333c78
朱备 <zbneversettle@gmail.com>
2024-01-19 09:51:31 +0800

    chore: Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
