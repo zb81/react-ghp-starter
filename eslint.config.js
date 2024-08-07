47b78d9c228a0977b92c6213f3cefa8f0c130a0f
朱备 <hi@zb81.icu>
2024-08-07 14:06:34 +0800

    chore: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,8 @@ import antfu from '@antfu/eslint-config'
 
 export default await antfu({
   react: true,
+  rules: {
+    'react-dom/no-missing-button-type': 'off',
+    'react/no-array-index-key': 'off',
+  },
 })
